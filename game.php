710f6c7bc399e25f6f16af1dc5a7b0bb9aee5169
Benjam Welker <benjamwelker@gmail.com>
2010-07-16 23:39:46 -0600

    preserved spaces in the in-game chat texts updated todo list

--- game.php
+++ game.php
@@ -61,6 +61,10 @@ if (is_array($chat_data)) {
 			$chat['username'] = '[deleted]';
 		}
 
+		// preserve spaces in the chat text
+		$chat['message'] = str_replace("\t", '    ', $chat['message']);
+		$chat['message'] = str_replace('  ', ' &nbsp;', $chat['message']);
+
 		$chat_html .= '
 				<dt class="'.$players[$chat['player_id']]['color'].'"><span>'.$chat['create_date'].'</span> '.$chat['username'].'</dt>
 				<dd'.($chat['private'] ? ' class="private"' : '').'>'.htmlentities($chat['message'], ENT_QUOTES, 'ISO-8859-1', false).'</dd>';
